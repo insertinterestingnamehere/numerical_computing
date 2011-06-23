e8a3f05daddf2a20436e64eeeab63812304d55b8
Ryan Grout <rgrout@byu.net>
2011-06-23 10:39:25 -0600

    fixed code to work with updated timer class

--- Source/temporal_complex.py
+++ Source/temporal_complex.py
@@ -2,20 +2,21 @@ import scipy as sp
 from timer import timer
 from matplotlib import pyplot as plt
 from scipy.linalg import lstsq
+from scipy import dot, arange
 
-i = sp.arange(1500, 2500+200, 200)
+i = arange(1500, 2500+200, 200)
 k = 1
 
 def multArray(A, B):
-    return sp.dot(A,B)    
+    return dot(A,B)    
 
 y = []
-for n in i:
-    with timer(loops=20) as t:
-        a = t.time(multArray, sp.rand(n,n), sp.rand(n,n))
-        y.append(a[0])
+with timer(loops=1, repeats=1) as t:
+    for n in i:
+        t.time(multArray, sp.rand(n,n), sp.rand(n,n))
+        y.append(t.results[0][0])
 
-X = sp.row_stack([sp.log(i), sp.ones_like(i)])
+X = sp.column_stack([sp.log(i), sp.ones_like(i)])
 sol = lstsq(X, sp.log(y))
 print sol[0][0]
 plt.loglog(i,y)
