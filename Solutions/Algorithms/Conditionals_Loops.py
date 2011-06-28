d0b7320873917a864868e559483321e27b6fbb90
Ryan Grout <rgrout@byu.net>
2011-06-28 10:22:29 -0600

    Solutions to Conditionals and Loops lab.
    Former-commit-id: fc2e0dd19e51391cfef4ee78988e1d833599235c

---
+++ Solutions/Algorithms/Conditionals_Loops.py
@@ -0,0 +1,28 @@
+def sum_loop(n):
+    """Sum all the numbers between 0 and -1 using a for loop"""
+    sum = 0
+    for i in range(n):
+        if i%2==1:
+            sum += i
+    return sum
+
+def sum_list(n):
+    return sum([i for i in range(n) if i%2==1])
+
+def expSS(x,i=100):
+    """Calculate e**x by scaling and squaring"""
+
+    from scipy import factorial
+
+    results = []
+    for val in x:
+        t,r = 0,0
+        val = float(val)
+        while (val>=10):
+            val = val/2
+            t += 2.0
+    
+        #calculate sum
+        results.append(sum([(val**z)/factorial(z) for z in range(i)])**t)
+    return results
+
