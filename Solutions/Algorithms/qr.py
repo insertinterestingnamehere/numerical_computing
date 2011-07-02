fe2fe3b8b96921f7ae8de310ded5364ac2b8b6ae
Ryan Grout <rgrout@byu.net>
2011-07-01 20:13:34 -0600

    change qr to deal with complex matrices Added functions to calculate det(X) and eigenvalues of X
    Former-commit-id: 06f799e03d02d2dddf55b9871325f04f90857929

--- Solutions/Algorithms/qr.py
+++ Solutions/Algorithms/qr.py
@@ -1,14 +1,15 @@
 import scipy as sp
-from scipy.linalg import norm
+from scipy.linalg import norm, det
 
-def Q(X):
+def QR(X):
     """Compute the Gram Schmidt of the column vectors in X
 
     The formula: x_k := x_k-<x_k, q_1>*q_1  (k=2,...,n)
     Then we normalize x_k"""
 
     #transpose so we are dealing with rows instead of columns
-    Q = X.T.copy().astype('float64')
+    #check types on X
+    Q = X.T.copy()
     nrows, ncols = Q.shape
     R = sp.zeros((nrows, ncols))
 
@@ -16,7 +17,27 @@ def Q(X):
         R[i,i] = norm(Q[i])
         Q[i] = Q[i]/R[i,i]
         for j in range(i+1,nrows):
-            R[i,j] = sp.dot(Q[j], Q[i])
+            R[i,j] = Q[j].dot(Q[i])
             Q[j] = Q[j]-(R[i,j]*Q[i])
 
     return Q.T, R
+
+def detQR(X):
+    Q, R = QR(X)
+    return la.det(Q)*la.det(R)
+    
+def eigvv(A, niter=50):
+    Qlist = [A]
+    x0 = sp.rand(A.shape[1])
+    for i in range(niter):
+        Q,R = QR(Qlist[-1])
+        A = Q.T.dot(A.dot(Q))        
+        Qlist.append(A)
+    
+    eigvals = sp.diag(Qlist[-1])
+    
+    L = sp.eye(Q.shape[0])    
+    for qm in Qlist:
+        L = sp.dot(L,qm)
+       
+    return eigvals, x0, L
