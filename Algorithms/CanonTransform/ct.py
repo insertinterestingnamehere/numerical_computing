035fc8d4d0e994b2d39758043a276984526dc77f
Kathleen Finlinson <phinies@gmail.com>
2012-07-31 14:05:23 -0600

    Removed some of the copying, and edited the docstrings.
    Former-commit-id: 8a2698ee195b5ad5f3823e0ea8c4ce6e27946c38

--- Algorithms/CanonTransform/ct.py
+++ Algorithms/CanonTransform/ct.py
@@ -2,44 +2,47 @@ from __future__ import division
 import scipy as sp
 import scipy.linalg as la
 
-
-
 def hqr(A):
-	""" Using Householder rotations, computes the QR decomposition for
-	an mxn matrix A, where m>=n.
-	input: A -- array, mxn
-	   getQ -- Boolean, tells whether to calculate Q (default True)
-	output: 
-		Q -- orthonormal mxm array
-		R -- upper triangular mxn array, s.t. QR = A"""
+	"""Finds the QR decomposition of A using Householder reflectors.
+	input: 	A, mxn array with m>=n
+	output: Q, orthogonal mxm array
+	        R, upper triangular mxn array
+	        s.t QR = A
+	"""
 	R = A.copy()
 	m,n = R.shape
 	Q = sp.eye(m,m)
 	for k in sp.arange(n-1):
-		v = R[k:m,k].copy() 
+		v = R[k:m,k].copy()
 		v[0] += sp.sign(v[0])*la.norm(v)
 		v = v/la.norm(v)
 		v = v.reshape(m-k,1)
 		P = sp.eye(m,m)
-		P[k:m,k:m] -= 2*sp.dot(v.copy(),v.copy().T)
+		P[k:m,k:m] -= 2*sp.dot(v,v.T)
 		Q = sp.dot(P,Q)
 		R = sp.dot(P,R)
 	return Q.T,R
 	
 def hess(A):
-	R = A.copy()
-	m,n = R.shape
+	"""Computes the upper Hessenberg form of A using Householder reflectors.
+	input:  A, mxn array
+	output: Q, orthogonal mxm array
+			H, upper Hessenberg
+			s.t. QHQ' = A
+	"""
+	H = A.copy()
+	m,n = H.shape
 	Q = sp.eye(m,m)
 	for k in sp.arange(n-2):
-		v = R[k+1:m,k].copy()
+		v = H[k+1:m,k].copy()
 		v[0] += sp.sign(v[0])*la.norm(v)
 		v = v/la.norm(v)
 		v = v.reshape(m-k-1,1)
 		P = sp.eye(m,m)
-		P[k+1:m,k+1:m] -= 2*sp.dot(v.copy(),v.copy().T)
+		P[k+1:m,k+1:m] -= 2*sp.dot(v,v.T)
 		Q = sp.dot(P,Q)
-		R = sp.dot(P,sp.dot(R,P.T))
-	return Q.T,R
+		H = sp.dot(P,sp.dot(H,P.T))
+	return Q.T,H
 	
 def gqr(A):
 	"""Finds the QR decomposition of A using Givens rotations.
@@ -49,8 +52,7 @@ def gqr(A):
 	        s.t QR = A
 	"""
 	def rotate(i,k,B):
-	# create the Givens rotation matrix G to zero out the 
-	# i,k entry of B
+	# create the Givens rotation matrix G to zero out the i,k entry of B
 		c,s,r = solve(B[k,k],B[i,k])
 		r = sp.sqrt(B[k,k]**2 + B[i,k]**2)
 		c = B[k,k]/r
@@ -65,7 +67,7 @@ def gqr(A):
 	B = A.copy()	
 	m,n = B.shape
 	G = sp.eye(m)
-	#cycle through each subdiagonal element of B, and zero it out
+	#cycle through each nonzero subdiagonal element of B, and rotate it to zero
 	for k in sp.arange(n-1):
 		for i in sp.arange(k+1,m):
 			if B[i,k] is not 0:
