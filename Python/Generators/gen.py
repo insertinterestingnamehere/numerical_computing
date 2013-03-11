0373c54baf61cf111eeaf93d044a33e5af1a6477
Ryan Grout <rgrout@math.byu.edu>
2013-03-11 16:09:54 -0600

    adding content to generator labs
    Added more content for combinations and permutations.  Solutions for
    same added as well.
    
    
    Former-commit-id: 6619d0d8b778d9d8648f998f883d24cc7d5a1880

--- Python/Generators/gen.py
+++ Python/Generators/gen.py
@@ -1,9 +1,19 @@
+from itertools import izip, compress
+
+def bin_combinations(n):
+    s = len(n)
+
+    b = 0
+    while b < 2**s:
+        yield [i for i, v in izip(n, reversed('{0:b}'.format(b))) if v == '1']
+        b += 1
 
 def gray_combinations(n):
-    t = [0]*n
-    yield t
+    s = len(n)
+    t = [0]*s
+    yield list(compress(reversed(n), t))
 
-    final = [1]+[0]*(n-1)
+    final = [1]+[0]*(s-1)
     while t != final:
         if sum(t)%2 == 0:
             t[-1] = (t[-1]+1) % 2
@@ -13,4 +23,26 @@ def gray_combinations(n):
                     t[-i-1] = (t[-i-1]+1)%2
                     break
 
-        yield t
+        yield list(compress(reversed(n), t))
+
+def quickperm(seq, r):
+    seq = list(set(seq))
+
+    n = len(seq)
+    p = range(n)
+
+    i = 1
+    while i < n:
+        p[i] -= 1
+        if i%2 == 1:
+            j = p[i]
+        else:
+            j = 0
+
+        seq[j], seq[i] = seq[i], seq[j]
+        i = 1
+        while p[i] == 0:
+            p[i] = i
+            i += 1
+        yield p
+
