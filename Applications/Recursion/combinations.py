a676a800377a594bd9c9eca8ed1e3de9aefc0827
Ryan Grout <rgrout@math.byu.edu>
2013-03-23 00:52:53 -0600

    update combinations
    This function should be rewritten.  It is broken right now.

--- Applications/Recursion/combinations.py
+++ Applications/Recursion/combinations.py
@@ -1,4 +1,5 @@
-import scipy as sp
+import numpy as np
+from scipy import misc
 
 def Combinations(values, k):
     """This function outputs all the possible combinations of k elements from the vector values"""
@@ -8,9 +9,9 @@ def Combinations(values, k):
     
     #Make input vectors column vectors
     if values.shape == (1,values.size):
-        values = sp.atleast2d(values).T.copy()
+        values = np.atleast2d(values).T.copy()
     
-    out = sp.array([]).reshape(0,1)
+    out = np.array([]).reshape(0,1)
     n = max(values.shape)
     if k == 1:
         out = values
@@ -20,10 +21,10 @@ def Combinations(values, k):
         #out = sp.zeros((nck, k))
         for i in range(n-(k-1)):
             #Calculate the number of possible combinations (to allow proper concatenation in the recursive call
-            nCombs = int(sp.misc.factorial(n-i)/(sp.misc.factorial(k-1)*sp.misc.factorial(n-i-(k-1))))
+            nCombs = int(misc.factorial(n-i)/(misc.factorial(k-1)*misc.factorial(n-i-(k-1))))
             #This is the recursive call
             
             print Combinations(values[i+1:], k-1).reshape((-1,1))
-            out = sp.r_[out, sp.c_[values[i]*sp.ones((nCombs,1)), Combinations(values[i+1:], k-1).reshape(-1,1)]]
+            out = np.r_[out, np.c_[values[i]*np.ones((nCombs,1)), Combinations(values[i+1:], k-1).reshape(-1,1)]]
             
     return out
