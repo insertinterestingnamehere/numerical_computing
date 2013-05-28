8983ce2d749b2e081884b550afa20630867bf450
Ryan Grout <rgrout@math.byu.edu>
2013-05-28 13:38:24 -0600

    organize image recognition solutions
    Split the solutions into functions.

--- Applications/ImageRecognition/imagerecognition.py
+++ Applications/ImageRecognition/imagerecognition.py
@@ -7,62 +7,80 @@ from sklearn.datasets import fetch_lfw_people
 from sklearn.grid_search import GridSearchCV
 from sklearn.decomposition import PCA
 
-digits = datasets.load_digits()
-data = digits.data
-target = digits.target
-N = len(target)
+def gen_digits_sets():
+        
+    digits = datasets.load_digits()
+    data = digits.data
+    target = digits.target
+    N = len(target)
 
-inds = random.sample(sp.arange(0,N),N)
-n_train = int(sp.floor(0.8*N))
-trainingdata = data[inds[0:n_train],:]
-trainingtarget = target[inds[0:n_train]]
-testdata = data[inds[n_train:]]
-testtarget = target[inds[n_train:]]
+    inds = random.sample(sp.arange(0,N),N)
+    n_train = int(sp.floor(0.8*N))
+    trainingdata = data[inds[0:n_train],:]
+    trainingtarget = target[inds[0:n_train]]
+    testdata = data[inds[n_train:]]
+    testtarget = target[inds[n_train:]]
+    
+    return trainingdata, testdata, trainingtarget, testtarget
 
-KNN = KNeighborsClassifier(n_neighbors=5)
-KNN.fit(trainingdata,trainingtarget)
-predictions = KNN.predict(testdata)
-n_misclassed = sum(predictions!=testtarget)
-misclassrate = n_misclassed/float(N-n_train)
-print misclassrate
+def misclass(trainingdata, testdata, trainingtarget, testtarget):
+        
+    KNN = KNeighborsClassifier(n_neighbors=5)
+    KNN.fit(trainingdata,trainingtarget)
+    predictions = KNN.predict(testdata)
+    n_misclassed = sum(predictions!=testtarget)
+    misclassrate = n_misclassed/float(len(testtarget)+len(trainingtarget)-len(trainingdata))
+    return misclassrate
 
-param_grid = {'C': [1e3,5e3,1e4,5e4,1e5],'gamma': [0.0001,0.0005,0.001,0.005,0.01,0.1],}
-clf = GridSearchCV(SVC(kernel='rbf',class_weight='auto'), param_grid)
-clf = clf.fit(trainingdata,trainingtarget)
+def svm_knn(trainingdata, testdata, trainingtarget, testtarget):
+    param_grid = {'C': [1e3,5e3,1e4,5e4,1e5], 
+                  'gamma': [0.0001,0.0005,0.001,0.005,0.01,0.1]}
+    clf = GridSearchCV(SVC(kernel='rbf',class_weight='auto'), param_grid)
+    clf = clf.fit(trainingdata,trainingtarget)
 
-predictions = clf.predict(testdata)
-print sum(predictions!=testtarget)/float(len(testtarget))
+    predictions = clf.predict(testdata)
+    return sum(predictions!=testtarget)/float(len(testtarget))
 
-people = fetch_lfw_people(min_faces_per_person=70,resize=0.4)
-n_samples,h,w = lfw_people.images.shape
+def gen_face_sets():
+    people = fetch_lfw_people(min_faces_per_person=70,resize=0.4)
+    n_samples,h,w = people.images.shape
 
-data = lfw_people.data
-n_features = data.shape[1]
+    data = people.data
+    n_features = data.shape[1]
 
-target = lfw_people.target
-target_names = lfw_people.target_names
-n_classes = target_names.shape[0]
+    target = people.target
+    target_names = people.target_names
+    n_classes = target_names.shape[0]
 
-N = len(target)
-inds = random.sample(sp.arange(0,N),N)
-n_train = int(sp.floor(0.8*N))
-trainingdata = data[inds[0:n_train],:]
-trainingtarget = target[inds[0:n_train]]
-testdata = data[inds[n_train:]]
-testtarget = target[inds[n_train:]]
 
-n_components = 150
-pca = PCA(n_components=n_components,whiten=True).fit(trainingdata)
-trainingdata_pca = pca.transform(trainingdata)
-testdata_pca = pca.transform(testdata)
+    N = len(target)
+    inds = random.sample(sp.arange(0,N),N)
+    n_train = int(sp.floor(0.8*N))
+    trainingdata = data[inds[0:n_train],:]
+    trainingtarget = target[inds[0:n_train]]
+    testdata = data[inds[n_train:]]
+    testtarget = target[inds[n_train:]]
+    return trainingdata, testdata, trainingtarget, testtarget
 
-clf = GridSearchCV(SVC(kernel='rbf',class_weight='auto'), param_grid)
-clf = clf.fit(trainingdata_pca,trainingtarget)
+def pca_trans(trainingdata, testdata):
+    n_components = 150
+    pca = PCA(n_components=n_components, whiten=True).fit(trainingdata)
+    trainingdata_pca = pca.transform(trainingdata)
+    return pca.transform(testdata)
+    
+def prob5(trainingdata, testdata, trainingtarget, testtarget):
+    trainingdata_pca = pca_trans(trainingdata, testdata)
+    param_grid = {'C': [1e3,5e3,1e4,5e4,1e5],
+                  'gamma': [0.0001,0.0005,0.001,0.005,0.01,0.1]}
+    clf = GridSearchCV(SVC(kernel='rbf',class_weight='auto'), param_grid)
+    clf = clf.fit(trainingdata_pca,trainingtarget)
 
-predictions = clf.predict(testdata_pca)
-print sum(predictions!=testtarget)/float(len(testtarget))
+    predictions = clf.predict(testdata_pca)
+    return sum(predictions!=testtarget)/float(len(testtarget))
 
-KNN = KNeighborsClassifier(n_neighbors=5)
-KNN.fit(trainingdata_pca,trainingtarget)
-predictions = KNN.predict(testdata_pca)
-print sum(predictions!=testtarget)/float(len(testtarget))
+def prob6(trainingdata, testdata, trainingtarget, testtarget):
+    trainingdata_pca = pca_trans(trainingdata, testdata)
+    KNN = KNeighborsClassifier(n_neighbors=5)
+    KNN.fit(trainingdata_pca,trainingtarget)
+    predictions = KNN.predict(testdata_pca)
+    return sum(predictions!=testtarget)/float(len(testtarget))
