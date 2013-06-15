e19626aed6c03b18d13a0b8734a337ad03d8846d
Ian <insertinterestingnamehere@gmail.com>
2013-06-14 23:51:26 -0600

    Sleight optimization of LU decomposition solution.
    Former-commit-id: 5719b588a89f992d8fe2687da23f801a87ba63cf

--- Algorithms/ElemMatrices/Elem_Matrices.py
+++ Algorithms/ElemMatrices/Elem_Matrices.py
@@ -3,18 +3,18 @@ import numpy as np
 def ref(A):
     for i in xrange(A.shape[0]):
         for j in xrange(i+1, A.shape[0]):
-            A[j] -= (A[j,i] / A[i,i]) * A[i]
+            A[j,i:] -= (A[j,i] / A[i,i]) * A[i,i:]
 
 def LU(A):
     U = A.copy()
     L = np.eye(A.shape[0])
     for i in xrange(A.shape[0]):
-        for j in xrange(i+1, A.shape[0]):
+        for j in xrange(i+1,A.shape[0]):
             #operation corresponding to left mult by
             #the elementary matrix desired
-            L[:,i] += (U[j,i] / U[i,i]) * L[:,j]
+            L[j,i] += (U[j,i] / U[i,i])
             #now we apply the change to U
-            U[j] -= (U[j,i] / U[i,i]) * U[i]
+            U[j,i:] -= (U[j,i] / U[i,i]) * U[i,i:]
     return L, U
 
 def LU_det(A):
