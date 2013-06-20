2d7a13cf81859a8b48d71bcf06828b4e23c6d205
Ian <insertinterestingnamehere@gmail.com>
2013-06-20 14:29:03 -0600

    Some more small changes to the LU decomposition lab.

--- Algorithms/LUdecomposition/LUdecomposition.py
+++ Algorithms/LUdecomposition/LUdecomposition.py
@@ -1,20 +1,20 @@
 import numpy as np
     
 def ref(A):
-    for i in xrange(A.shape[0]):
+    for i in xrange(A.shape[0]-1):
         for j in xrange(i+1, A.shape[0]):
             A[j,i:] -= (A[j,i] / A[i,i]) * A[i,i:]
 
 def LU(A):
     U = A.copy()
     L = np.eye(A.shape[0])
-    for i in xrange(A.shape[0]):
-        for j in xrange(i+1,A.shape[0]):
+    for i in xrange(A.shape[0]-1):
+        for j in xrange(i+1, A.shape[0]):
             #operation corresponding to left mult by
             #the elementary matrix desired
-            L[j,i] += (U[j,i] / U[i,i])
+            L[j,i] = (U[j,i] / U[i,i])
             #now we apply the change to U
-            U[j,i:] -= (U[j,i] / U[i,i]) * U[i,i:]
+            U[j,i:] -= L[j,i] * U[i,i:]
     return L, U
 
 def LU_det(A):
