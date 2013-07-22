b9cbcb3d9287790734c61dec9027df945d77c4c9
Jeff Hendricks <jjhendricks@gmail.com>
2013-07-22 12:33:20 -0600

    Small changes to fft labs and plot files.

--- Algorithms/FFT/fft_plots.py
+++ Algorithms/FFT/fft_plots.py
@@ -50,46 +50,7 @@ def plot_tada_spec_left():
     plt.figure()
     plt.plot(f[0:f.shape[0]/2,:])
     plt.savefig('tadaspec2.pdf')
-    
-#plots Noisysignal1.wav
-#TODO NEG    
-def plot_noise():
-    plt.close('all')
-    rate, sig = wavfile.read('Noisysignal1.wav')
-    plt.figure()
-    plt.plot(sig)
-    plt.show()
-#    plt.savefig('noise.pdf')
-    
-#plots lect half of spectrum of 
-#Noisysignal1.wav    
-def plot_noise_spec():
-    plt.close('all')
-    rate, sig = wavfile.read('Noisysignal1.wav')
-    sig = sp.float32(sig)
-    fsig = anfft.fft(sig.T).T
-    f = sp.absolute(fsig)
-    plt.figure()
-    plt.plot(f[0:f.shape[0]/2])
-    plt.savefig('noisyspec.pdf')
-    
-#plots cleaned noisy signal
-#TODO NEG    
-def plot_cleaned_signal():
-    plt.close('all')
-    rate,data = wavfile.read('Noisysignal1.wav')
-    fsig = sp.fft(data,axis = 0)
-    for j in xrange(10000,20000):
-        fsig[j]=0
-        fsig[-j]=0
 
-    newsig=sp.ifft(fsig)
-    newsig = sp.real(newsig)
-    newsig = sp.int16(newsig/sp.absolute(newsig).max() * 32767)    
-    plt.figure()
-    plt.plot(newsig)
-    plt.savefig('Cleanedsignal.pdf')
-    
 def plot_saw_spec():
     plt.close('all')
     rate, data = wavfile.read('saw.wav')
@@ -125,3 +86,4 @@ def plot_down_saw_spec_correct():
     f = sp.absolute(fout)
     plt.plot(f[0:f.shape[0]/2])
     plt.savefig('sawdownspec.pdf')
+    
