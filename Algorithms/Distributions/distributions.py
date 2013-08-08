fc46b07095ba3b61b4a7a91dd370079619e42b53
Jared Fisher <iamjet56@gmail.com>
2013-08-08 16:09:40 -0600

    Lab: Statistical Distributions
    Review common distributions in statistics.
    
    Former-commit-id: cb968860c6bb7f0872c6c838a9f2f303dab7603a

---
+++ Algorithms/Distributions/distributions.py
@@ -0,0 +1,35 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+N=10000
+mathematics = np.random.normal(500,100,N)
+reading = np.random.normal(500,100,N)
+writing = np.random.normal(500,100,N)
+SATscore = mathematics + reading + writing
+
+plt.hist(SATscore)
+plt.show()
+
+np.std(SATscore)
+np.sqrt(3*10000)
+np.var(SATscore)
+np.mean(SATscore)
+
+#problem 2
+sig = np.ones((3,3)) *5000
+sig[0,0]=10000
+sig[1,1]=10000
+sig[2,2]=10000
+sig[1,2]=7000
+sig[2,1]=7000
+
+mvnscores = np.random.multivariate_normal(mu,sig,10000)
+SATscores = np.sum(mvnscores,axis=1)
+
+plt.hist(SATscores,bins=100)
+plt.show()
+
+np.mean(SATscores)
+np.var(SATscores)
+np.std(SATscores)
+np.sqrt(64000)
