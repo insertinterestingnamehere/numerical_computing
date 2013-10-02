3807a6993b7d3d6359c48b7919b536c57a60ddb4
Abraham Frandsen <abefrandsen@gmail.com>
2013-10-02 10:56:43 -0600

    Made comments into docstrings in the solutions file for the Affine Transformations lab.
    Former-commit-id: c3bf7f93dc7ba52f36af5db95b0a9bb29e231987

--- Algorithms/ChangeBasis/solutions.py
+++ Algorithms/ChangeBasis/solutions.py
@@ -9,8 +9,14 @@ import math
 from matplotlib import pyplot as plt
 
 def plotNewOld(new, old):
-    ''' This plotting script gives better results than the one provided in the lab
-        text. Please use this to plot your figures.
+    ''' 
+    This plotting script gives better results than the one provided in the lab
+    text. Please use this to plot your figures.
+    Inputs:
+    new -- a (2,n) numpy array containing the transformed x-coordinates on the 
+            first row, y-coordinates on the second row.
+    old -- a (2,n) numpy array containing the original x-coordinates on the first
+            row, y-coordinates on the second row.
     '''
     # Find the largest and smallest x- and y-values, used to set the size of the axes
     x_max = np.max([np.max(old[0]), np.max(new[0])])
@@ -35,41 +41,53 @@ def plotNewOld(new, old):
     # Display result
     plt.show()
 
-'''
-    Problem 1
-    Implement the function declaration below. The pts argument has shape (2,n),
-    where the first row represents the x-coordinates and the second row the
-    y-coordinates. The stretch argument is simply a one-dimensional numpy array
-    with two entries--the first gives the scaling factor in the x-direction,
-    and the second gives the scaling factor in the y-direction. You should
-    implement the dilation as a matrix multiplication. Use np.diag to create
-    an appropriate matrix from the stretch argument. Then use np.dot to perform
-    the multiplication. Return the result.
-'''
+
 def dilation(pts, stretch):
+    '''
+    Apply a dilation to an array of points. You should
+    implement the dilation as a matrix multiplication. Use np.diag to create
+    an appropriate matrix from the stretch argument.
+    Inputs:
+        pts -- a (2,n) array of points, x-coordinates on the first row, y-coordinates
+                on the second row.
+        stretch -- a length 2 NumPy array giving the dilation factor in the x- and
+                    y-direction entrywise, respectively
+    Returns:
+        a (2,n) array representing the dilated points.
+    '''
     return np.dot(np.diag(stretch), pts)
 
-'''
-    Problem 2
-    Implement the function declaration below. The pts argument is the same as above.
-    The angle argument is a floating point number representing the angle of rotation
-    in radians. Inside of the function you will need to create the appropriate
-    rotation matrix, and then perform the requisite matrix multiplication, and
-    return the result.
-'''
+
 def rotation(pts, angle):
+    '''
+    Apply a rotation to an array of points. Inside of the function you will need to 
+    create the appropriate rotation matrix, and then perform the requisite matrix 
+    multiplication, and return the result.
+    Inputs:
+        pts -- a (2,n) array of points, x-coordinates on the first row, y-coordinates
+                on the second row.
+        angle -- a scalar value indicating the angle of rotation in radians.
+    Returns:
+        a (2,n) arrary representing the rotated points.
+    '''
     a = math.cos(angle)
     b = math.sin(angle)
     rot = np.array([a,-b,b,a]).reshape((2,2))
     return np.dot(rot, pts)
 
-'''
-    Problem 3
-    Implement the function declaration below. You will need an if statement to deal with
-    the possible values of the direction argument (0 means horizontal, 1 means vertical).
-    The c argument is the amount of shearing.
-'''
+
 def shear(pts, c, direction):
+    '''
+    Apply a shear to an array of points. You will need an if statement to deal with
+    the possible values of the direction argument.
+    Inputs:
+        pts -- a (2,n) array of points, x-coordinates on the first row, y-coordinates
+                on the second row.
+        c -- a scalar value indicating the amount of shearing
+        direction -- value of 0 means horizontal shear, 1 means vertical shear.
+    Returns:
+        a (2,n) array holding the sheared points.
+    '''
     shear_mat = np.eye(2)
     if direction==0:
         shear_mat[0,1]=c
@@ -77,46 +95,64 @@ def shear(pts, c, direction):
         shear_mat[1,0]=c
     return np.dot(shear_mat,pts)
 
-'''
-    Problem 4
-    Implement the function declaration below. The axis argument is a one-dimensional
-    array with two entries, which represents a vector pointing in the direction of
-    the axis of rotation. You will need to use this array to create the appropriate
-    reflection matrix, and then perform matrix multiplication.
-'''
+
 def reflection(pts, axis):
+    '''
+    Apply a reflection to an array of points. You will need to use this array to create 
+    the appropriate reflection matrix, and then perform matrix multiplication.
+    Inputs:
+         pts -- a (2,n) array of points, x-coordinates on the first row, y-coordinates
+                on the second row.
+         axis -- a length two array representing a vector pointing in the direction 
+                  of the axis of rotation.
+    Returns:
+        a (2,n) array containing the reflected points.
+    '''
     a = ((axis**2)*np.array([1,-1])).sum()
     b = 2*axis.prod()
     c = (axis**2).sum()
     ref = (np.array([a,b,b,-a]).reshape((2,2)))/float(c)
     return np.dot(ref,pts)
 
-'''
-    Problem 5
-    Implement the function declaration below. The shift argument is an array with two
-    entries. To perform the translation, you simply need to add the shift array to
-    the pts array, but before doing this, use the .reshape method on the shift array
-    to ensure that it has the same shape as the example given in the text.
-'''
+
 def translation(pts, shift):
+    '''
+    Apply a translation to an array of points. To perform the translation, you simply 
+    need to add the shift array to the pts array, but before doing this, use the 
+    .reshape method on the shift array to ensure that it has the same shape as the 
+    example given in the text.
+    Inputs:
+        pts -- a (2,n) array of points, x-coordinates on the first row, y-coordinates
+                on the second row.
+        shift -- a length two array representing the amount of translation in each direction.
+    '''
     return pts + shift.reshape((2,1))
 
-'''
-    Problem 6
-    Implement the function declaration below. The t argument indicates time (in seconds),
-    the w argument indicates angular velocity (in radians/seconds) and the v argument
-    indicates directional speed (in meters/second). Hint: you will use a composition of
-    rotation and translation in this problem. Make sure the function returns an array
-    indicating the new position of p1 at time t.
-'''
+
 def trajectory(t,w,v):
+    '''
+    Calculate the trajectory of an object rotating about a movign point. 
+    Hint: you will use a composition of rotation and translation in this problem. 
+    Make sure the function returns an array indicating the new position of p1 at 
+    time t. 
+    Inputs:
+        t -- time (in seconds)
+        w -- angular velocity of object (radians/seconds)
+        v -- direction speed of center of rotation (meters/second)
+    Returns:
+        the position of the rotating object at time t.
+    '''
     angle = t*w
     p = np.array([[0],[1]])
     shift = t*v*0.5*np.array([math.sqrt(2),math.sqrt(2)])
     return translation(rotation(p, angle), shift)
 
-# Use this function to visualize the trajectory of the particle
+
 def plotTrajectory():
+    '''
+    Plot the trajectory of the rotating particle as calculated in the trajectory
+    function. 
+    '''
     times = np.arange(0,10,.1)
     pos = np.zeros((2,len(times)))
     for i in range(len(times)):
@@ -124,11 +160,13 @@ def plotTrajectory():
     plt.scatter(pos[0], pos[1])
     plt.show()
     
-# The following code will be used to test your solutions. This should be the only code
-# in this script that actually runs. DO NOT ALTER! To make sure you pass-off, run this
-# file from command line by typing:   python solutions.py
-# You should see a series of plots pop up, reflecting the various affine transformations
-# that you implemented.
+'''
+The following code will be used to test your solutions. This should be the only code
+in this script that actually runs. DO NOT ALTER! To make sure you pass-off, run this
+file from command line by typing:   python solutions.py
+You should see a series of plots pop up, reflecting the various affine transformations
+that you implemented.
+'''
 
 x, y = np.load('horse.npy')
 pts = np.array([x,y])
