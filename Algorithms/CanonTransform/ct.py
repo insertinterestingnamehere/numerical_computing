da2eb3e2048be4cbb1a92c464bfdc952f99d7020
Ian Henriksen <insertinterestingnamehere@gmail.com>
2013-10-04 22:18:19 -0600

    Removed givens solution form Householder lab.
    Former-commit-id: b232caffb2c72e7f068817a97c439cd1981942f5

--- Algorithms/CanonTransform/ct.py
+++ Algorithms/CanonTransform/ct.py
@@ -59,35 +59,3 @@ def hess(A):
 		Q = P.dot(Q)
         H = P.dot(H).dot(P.T)
 	return Q.T, H
-	
-def gqr(A):
-	"""Finds the QR decomposition of A using Givens rotations.
-	input: 	A, mxn array with m>=n
-	output: Q, orthogonal mxm array
-	        R, upper triangular mxn array
-	        s.t QR = A
-	"""
-	def rotate(i, k, B):
-	# create the Givens rotation matrix G to zero out the i,k entry of B
-		c,s,r = la.solve(B[k,k], B[i,k])
-		r = np.sqrt(B[k,k]**2 + B[i,k]**2)
-		c = B[k,k] / r
-		s = - B[i,k] / r
-		G = np.eye(m)
-		G[i,i] = c
-		G[k,k] = c
-		G[k,i] = -s
-		G[i,k] = s
-		return G
-	
-	B = A.copy()	
-	m, n = B.shape
-	G = np.eye(m)
-	#cycle through each nonzero subdiagonal element of B, and rotate it to zero
-	for k in xrange(n-1):
-		for i in xrange(k+1, m):
-			if B[i,k] != 0:
-				H = rotate(i, k, B)
-				B = H.dot(B)
-				G = H.dot(G)
-	return G.T, B
