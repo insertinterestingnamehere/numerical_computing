0f38d6dc0abf3ee794134e15d72eb3ba26b9e9b6
Ian Henriksen <insertinterestingnamehere@gmail.com>
2013-10-04 22:01:18 -0600

    Replaced householder qr solution with an updated solution.
    Former-commit-id: 4a10b84aeb5071c9059d3dbcfffba600646ec016

--- Algorithms/CanonTransform/ct.py
+++ Algorithms/CanonTransform/ct.py
@@ -2,26 +2,42 @@ import numpy as np
 from scipy import linalg as la
 
 def hqr(A):
-	"""Finds the QR decomposition of A using Householder reflectors.
-	input: 	A, mxn array with m>=n
-	output: Q, orthogonal mxm array
-	        R, upper triangular mxn array
-	        s.t QR = A
-	"""
-	R = A.copy()
-	m, n = R.shape
-	Q = np.eye(m)
-	for k in xrange(n-1):
-		v = R[k:,k].copy()
-		v[0] += np.sign(v[0]) * la.norm(v)
-		v /= la.norm(v)
-		v = v.reshape(m-k, 1)
-		P = np.eye(m)
-		P[k:,k:] -= 2 * v.dot(v.T)
-		Q = P.dot(Q)
-		R = P.dot(R)
-	return Q.T,R
-	
+    """Finds the QR decomposition of A using Householder reflectors.
+    input: 	A, mxn array with m>=n
+    output: Q, orthogonal mxm array
+            R, upper triangular mxn array
+            s.t QR = A
+    """
+    # This is just a pure Python implementation.
+    # It's not well optimized, but it should
+    # have the right asymptotic speed.
+    # initialize Q and R
+    # start Q as an identity
+    # start R as a C-contiguous copy of A
+    # take a transpose of Q to start out
+    # so it is C-contiguous when we return the answer
+    Q = np.eye(A.shape[0]).T
+    R = np.array(A, order="C")
+    # initialize m and n for convenience
+    m, n = R.shape
+    # avoid reallocating v in the for loop
+    v = np.empty(A.shape[1])
+    for k in xrange(n):
+        # get a slice of the temporary array
+        vk = v[k:]
+        # fill it with corresponding values from R
+        vk[:] = R[k:,k]
+        # add in the term that makes the reflection work
+        vk[0] += copysign(la.norm(vk), vk[0])
+        # normalize it so it's an orthogonal transform
+        vk /= la.norm(vk)
+        # apply projection to R
+        R[k:,k:] -= 2 * np.outer(vk, vk.dot(R[k:,k:]))
+        # Apply it to Q
+        Q[k:,:] -= 2 * np.outer(vk, vk.dot(Q[k:,:]))
+    # note that its returning Q.T, not Q itself
+    return Q.T, R
+
 def hess(A):
 	"""Computes the upper Hessenberg form of A using Householder reflectors.
 	input:  A, mxn array
@@ -31,13 +47,13 @@ def hess(A):
 	"""
 	H = A.copy()
 	m, n = H.shape
-	Q = np.eye(m, m)
+	Q = np.eye(m)
 	for k in xrange(n-2):
 		v = H[k+1:,k].copy()
 		v[0] += np.sign(v[0]) * la.norm(v)
 		v /= la.norm(v)
 		v = v.reshape(m-k-1, 1)
-		P = np.eye(m, m)
+		P = np.eye(m)
 		P[k+1:,k+1:] -= 2 * v.dot(v.T)
 		Q = P.dot(Q)
         H = P.dot(H).dot(P.T)
