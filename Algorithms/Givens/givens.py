82b2de483f46f36443ee7999e75c0cc5820569a1
Ian Henriksen <insertinterestingnamehere@gmail.com>
2013-10-17 15:04:51 -0600

    Added solution to modifed givens Hessenberg problem.
    Former-commit-id: d12ccf1acca79f0415300e6c6b1c51ac8151d817

--- Algorithms/Givens/givens.py
+++ Algorithms/Givens/givens.py
@@ -80,3 +80,54 @@ def givens_hess(A, tol=1E-15):
             Q[j:j+2,:j+2] = G.dot(Q[j:j+2,:j+2])
     # Return Q^T and R.
     return Q.T, R
+
+def givens_hess_modified(A, tol=1E-15):
+    # Make R a copy of A and Q an
+    # identity array of the appropriate size.
+    R = np.array(A, order="C")
+    # Store each Givens Rotation along
+    # the first axis of Q
+    Q = np.empty((A.shape[0]-1, 2, 2))
+    # Iterate along first subdiagonal.
+    # Run index along columns.
+    for j in xrange(A.shape[0]-1):
+        # If the leading entry of this row is
+        # not zero (i.e. if its absolute value
+        # is within a given tolerance):
+        if tol <= abs(R[j+1,j]):
+            # Compute c and s using the entry
+            # in the current row and column
+            # and the entry immediately above it.
+            c = R[j,j]
+            s = - R[j+1,j]
+            n = sqrt(c**2 + s**2)
+            c /= n
+            s /= n
+            # Use c and s to construct the
+            # needed Givens rotation
+            Q[j,0,0] = c
+            Q[j,1,1] = c
+            Q[j,0,1] = -s
+            Q[j,1,0] = s
+            # Apply rotation to proper portion of R.
+            R[j:j+2,j:] = Q[j].dot(R[j:j+2,j:])
+    # Return Q and R.
+    return Q, R
+
+def apply_QT(Q, B):
+    # array to return
+    C = np.array(B, order="C")
+    # apply Givens rotations in order
+    for i in xrange(Q.shape[0]):
+        C[i:i+2] = Q[i].dot(C[i:i+2])
+    return C
+
+def apply_Q(Q, B):
+    # array to return
+    C = np.array(B, order="C")
+    # Apply transpose of each Givens
+    # rotation to the corresponding
+    # portion of C in reversed order.
+    for i in xrange(Q.shape[0]-1, -1, -1):
+        C[i:i+2] = Q[i].T.dot(C[i:i+2])
+    return C
