334dcbd664ac91b2155a6e2e3ada12584d3dac04
Ian Henriksen <insertinterestingnamehere@gmail.com>
2013-10-23 22:17:08 -0600

    Added comments to test script.
    Former-commit-id: 3c2be760f5f1148607ad4291ece6b4734bcc8fdb

--- Python/cython_wrapping/ctridiag_test.py
+++ Python/cython_wrapping/ctridiag_test.py
@@ -1,7 +1,14 @@
 import numpy as np
 from numpy.random import rand
-from cytridiag import cytridiag as ct
+# Import the new module.
+from cython_ctridiag import cytridiag as ct
 
 a, b, c, x = rand(9), rand(10), rand(9), rand(10)
+# Call the function.
 ct(a, b, c, x)
+# The printed values should be somewhat different
+# than the usual output from rand().
+# There should probably be some negative values
+# and definitely no extemely large or extremely
+# small values.
 print x
