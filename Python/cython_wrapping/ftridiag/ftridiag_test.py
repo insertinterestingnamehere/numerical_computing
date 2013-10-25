1371db2a51305d7fd492b9e130d59a19fce3b0f6
Ian Henriksen <insertinterestingnamehere@gmail.com>
2013-10-24 18:08:52 -0600

    Added a warning about passing strided arrays as pointers. Also fixed a naming issue in the ftridiag test script.
    Former-commit-id: 2dc893e41a984eceeec5e19637f795d9270cff97

--- Python/cython_wrapping/ftridiag/ftridiag_test.py
+++ Python/cython_wrapping/ftridiag/ftridiag_test.py
@@ -1,6 +1,6 @@
 import numpy as np
 from numpy.random import rand
-from cytridiag import cytridiag as ct
+from cython_ftridiag import cytridiag as ct
 
 a, b, c, x = rand(9), rand(10), rand(9), rand(10)
 ct(a, b, c, x)
