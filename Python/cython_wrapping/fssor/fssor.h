ff1e5c845d57eb64ddcb7d443d7e9b485f026adf
Ian Henriksen <insertinterestingnamehere@gmail.com>
2013-11-07 17:11:17 -0700

    Added fssor.h.

---
+++ Python/cython_wrapping/fssor/fssor.h
@@ -0,0 +1,2 @@
+extern void fssor(double* U, int* m, int* n, double* omega, double* tol, int* maxiters, int* info);
+// Notice that we passed n, m, omega, tol, maxiters, and info as pointers as well.
