3434dc480d9d7b44be53276209302e7a95e2b58a
Ryan Grout <rgrout@math.byu.edu>
2013-12-24 12:03:24 -0700

    rewrote Simplex solver
    Rewrote simplex solver to much clearer and more concise.
    Follows the method explained just about everywhere (uses row operations
    instead of matrix multiplication).

--- Algorithms/simplex/Simplex.py
+++ Algorithms/simplex/Simplex.py
@@ -1,113 +1,104 @@
 import numpy as np
 
-def simplex(T):
-   # choose the first pivot: this sets exit to
-   # 1 if finished or 3 if unbounded
-   i, j, exit = choosepivot(T)
-   while exit == 0:
-       pivot(T,i,j)
-       i, j, exit = choosepivot(T)
-   return i, j, exit
+class InfeasibleSystem(Exception):
+    pass
 
-def createTableau(c, A, b):
-    m,n = A.shape
-    s = m + n + 1;    #m is number of constraints, n is number of variables 
-    T = np.zeros((s,s))
-    T[0, 1:n+1] = c.T
-    I = np.eye(n)
-    T[1:n+1, 1:n+1] = I.squeeze()
-    T[n+1:s, 0] = b.squeeze()
-    T[n+1:s, 1:n+1] = -A.squeeze()
-    return T
+class UnboundedSystem(Exception):
+    pass
 
-def choosepivot(T):
-   s = T.shape
-   finished = True
-   for k in range(1,s[0]):
-      if T[0][k] > 0:
-         finished = False
-         break
-   if finished == True:
-      exitvalue = 1
-      return 0,0,exitvalue
-   objective = T[0]
-   m = 0
-   for k in range(1,s[1]):
-      if objective[k] > m and objective[k] > 0:
-         m = objective[k]
-         i = k
-   first = True
-   for k in range(1,s[0]):
-      if T[k][0] != 0 and T[k][i] != 0 and first == True:
-         m = T[k][0]/T[k][i]
-         j = k
-         first = False
-      elif T[k][0] != 0 and T[k][i] != 0:
-         r = T[k][0]/T[k][i]
-         if m < r and r <= 0:
-            m = r       
-            j = k
-   exitvalue = 0
-   print i,j
-   return i, j, exitvalue
+class SimplexSolver(object):
+    
+    class _MaximalValue(Exception):
+        pass
+    
+    def __init__(self, c, A, b):
+        self.c = np.asarray(c)
+        self.A = np.asarray(A)
+        self.b = np.asarray(b)
+        
+        #check the system for feasibility
+        if (b.size, c.size) != A.shape:
+            raise InfeasibleSystem
+            
+        self.generateTab()
+        
+    def generateTab(self):
+        nr, nc = self.A.shape
+        self.tab = np.zeros((nr+1,nc+nr+2))
+        
+        _t = 1 + nc
+        self.tab[0,1:(_t)] = -self.c
+        self.tab[0,-1] = 1
+        self.tab[1:,0] = self.b
+        self.tab[1:,1:(_t)] = self.A
+        self.tab[1:,(_t):-1] = np.eye(nr)
+        
+        self.vars = range(nc, nc+nr)+range(0, nc)
+        self.nbasic = nr
+        #self.basic = range(nc, nc+nr)
+        #self.nonbasic = range(0, nc)
+        
+    def _pivot_col(self):
+        '''Determine the index of the next pivot column
+        
+        Do so by finding the most negative coefficient in the objective function.'''
+        
+        return self.tab[0,1:].argmin()
+        
+    def _pivot_row(self, col):
+        '''Determine the index of the next pivot row'''
+        
+        #do a ratio test to find smallest non-negative ratio
+        
+        ratios = self.tab[1:,0]/self.tab[1:,col+1]
+        print ratios
+        ratios[ratios<0] = np.inf
+        return ratios.argmin()
+    
+    def _pivot(self):
+        col = self._pivot_col()        
+        if self.tab[0, 1+col] < 0:
+            #check if unbounded
+            if (self.tab[1:,1+col] > 0).any():
+                row = self._pivot_row(col)
 
-def pivot(T,i,j):
-   U = T.copy()
-   vector = U[j]
-   vector[j] = -1
-   m = float(vector[i]/(-1))
-   row = np.divide(vector,m)
-   V = np.outer(U[:,i], row)
-   T = T + V
-   return T
-
-def isfeasible(T):
-   return exit
-
-
-def main(A,b,c):
-   exitvalue = 0
-   T = createTableau(c,A,b)
-   print T
-   i,j,exitvalue = choosepivot(T)
-   iteration = 0
-   while exitvalue == 0:
-      T = pivot(T,i,j)
-      iteration += 1
-      i,j,exitvalue = choosepivot(T)
-      print T
-   return T[0][0], iteration, exitvalue
-
-
-n = 3
-A = np.zeros(shape=(n, n))
-b = []
-c = []
-for i in range(n):
-   c.append(2**(n-1-i))
-   b.append(5**(i+1))
-   row = []
-   A[i][i] = 1
-   k = 2
-   for j in range(i-1,-1,-1):
-      A[i][j] = 2**k
-      k += 1
-
-
-
-
-A = np.array(A)
-b = np.array(b)
-c = np.array(c)
-##print A
-##print b
-##print c
-
-##T = createTableau(c,A,b)
-##print T
-##i,j,exitvalue = choosepivot(T)
-##print i,j
-##print pivot(T,i,j)
-
-print main(A,b,c)
-
+                #swap basic and nonbasic variables
+                icol, irow = self.vars.index(col), self.vars.index(row)
+                print "Vars index {} {}".format(icol, irow)
+                self.vars[icol], self.vars[row] = self.vars[row], self.vars[icol]
+                
+                print self.vars
+                self._reduceform(row, col)
+            else:
+                raise UnboundedSystem()
+        else:
+            raise self._MaximalValue(self.getState());
+            
+    def _reduceform(self, row, col):
+        '''Reduce the col to elementary vector using where row has the 1 element'''
+        
+        row1 = row + 1
+        col1 = col + 1
+        #divide row by tab[1+row,1+col]
+        #this gets the one
+        self.tab[row1] /= self.tab[row1, col1]
+        
+        #reduce the rest of the column to zeros
+        _row = self.tab[row1]
+        for i in xrange(self.tab.shape[0]):
+            if i == row1:
+                continue
+            self.tab[i] -= _row*self.tab[i, col1]
+            
+        
+    def __repr__(self):
+        return "Simplex Solver: Maximize {}\nSubject to\n{}\nConstraints: {}".format(self.c, self.A, self.b)
+    
+    def __str__(self):
+        return str(self.tab)
+        
+    def getState(self):
+        b = dict(zip(self.vars[:self.nbasic], self.tab[1:,0]))
+        nb = {k:0 for k in self.vars[self.nbasic:]}
+        
+        return self.tab[0,0], b, nb
