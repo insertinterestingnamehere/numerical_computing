ac3cb5563549d29ad3a0c0951806abe917a60f76
Ian Henriksen <insertinterestingnamehere@gmail.com>
2014-01-14 10:46:25 -0700

    Restructured existing solution files in the linesweep lab.

--- Algorithms/linesweep/voronoi_solutions.py
+++ Algorithms/linesweep/voronoi_solutions.py
@@ -3,7 +3,6 @@ import scipy.spatial as st
 from matplotlib import pyplot as plt
 from math import sqrt
 import heapq as hq
-#from markov_solutions import findpath
 from edge_intersections import edge_intersections, inside
 
 # farthest point problem
@@ -17,26 +16,6 @@ def farthest(pts, xlims, ylims, n):
     Q += [(KD.query(pt)[0],pt) for pt in edge_intersections(V, xlims, ylims)[0]]
     Q += [(KD.query(pt)[0],(x,y)) for x in xlims for y in ylims]
     return np.array([pair[1] for pair in hq.nlargest(n,Q)])
-
-# obstacle navegation problem
-# this problem is commented out
-def make_adj(V, xlims, ylims, threshold):
-    ins = lambda pt: inside(pt, xlims, ylims)
-    size = V.vertices.shape[0]
-    A = np.zeros((size,size),dtype=bool)
-    for edge, centers in zip(V.ridge_vertices, V.ridge_points):
-        if -1 not in edge:
-            if threshold <= sqrt(np.sum((V.points[centers[0]]-V.points[centers[1]])**2))/2.:
-                A[edge[0],edge[1]] = True
-    A += A.T
-    return A
-
-def plot_path(V, i, j, xlims, ylims, threshold):
-    A = make_adj(V, xlims, ylims, threshold)
-    path = V.vertices[findpath(i,j,A)]
-    st.voronoi_plot_2d(V)
-    plt.plot(path[:,0], path[:,1])
-    plt.show()
 
 # triangulation of the unit squre problem
 
