b518114df3c059957e0dbe84f8abad52e9c4c9ef
Jared Webb <Jwebb@savvysherpa.com>
2014-01-28 11:20:14 -0500

    Testing a change to solution.py

--- Algorithms/GaussQuad/solution.py
+++ Algorithms/GaussQuad/solution.py
@@ -3,4 +3,4 @@
 #Problem 1:  Write a function that will accept a function and limits of integration.  Use the change of interval formula and draw the graph of the function on [-1, 1]
 
 def change_interval(f, a, b):
-	
+	MAKING A CHANGE	
