61801334363becb7a98dcdc0602ffbd72c0413b7
Ian Henriksen <insertinterestingnamehere@gmail.com>
2014-02-24 16:28:34 -0700

    Added a new section and problem on complex visualization.

--- Algorithms/complexnumbers/figures.py
+++ Algorithms/complexnumbers/figures.py
@@ -2,6 +2,7 @@ from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import pyplot as plt
 import scipy as sp
 import numpy as np
+from solutions import get_vals
 
 cmap = plt.cm.coolwarm
 cmap_r = plt.cm.coolwarm_r
@@ -82,8 +83,37 @@ def arctan_riemann_surface():
     ax.plot_surface(X, Y, Z-sp.pi, rstride=1, cstride=1, linewidth=0, cmap=cmap)
     plt.savefig('arctan_riemann_surface.pdf', bbox_inches='tight', pad_inches=0)
 
+def poly_color_plot_real(p, res=101):
+    X, Y, vals = get_vals(p, (-1, 1), (-1, 1), res=res)
+    plt.pcolormesh(X, Y, vals.real)
+    plt.savefig("poly_color_plot_real.pdf")
+
+def poly_color_plot_imag(p, res=101):
+    X, Y, vals = get_vals(p, (-1, 1), (-1, 1), res=res)
+    plt.pcolormesh(X, Y, vals.imag)
+    plt.savefig("poly_color_plot_imag.pdf")
+
+def poly_surface_plot_real(p, res=101):
+    X, Y, vals = get_vals(p, (-1, 1), (-1, 1), res)
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
+    ax.plot_surface(X, Y, vals.real)
+    plt.savefig("poly_surface_plot_real.pdf")
+
+def poly_surface_plot_imag(p, res=101):
+    X, Y, vals = get_vals(p, (-1, 1), (-1, 1), res)
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
+    ax.plot_surface(X, Y, vals.imag)
+    plt.savefig("poly_surface_plot_imag.pdf")
+
 if __name__=='__main__':
     sqrt_riemann_surface_1()
     sqrt_riemann_surface_2()
     log_riemann_surface()
     arctan_riemann_surface()
+    p = np.poly1d([1, 0, -1])
+    poly_color_plot_real(p)
+    poly_color_plot_imag(p)
+    poly_surface_plot_real(p)
+    poly_surface_plot_imag(p)
--- Algorithms/complexnumbers/solutions.py
+++ Algorithms/complexnumbers/solutions.py
@@ -7,7 +7,53 @@ except:
     from matplotlib import pyplot as plt
     from mpl_toolkits.mplot3d import Axes3D
 
-def plot_real_part_matplotlib(n, res=101):
+def get_vals(f, xbounds, ybounds, res):
+    x = np.linspace(xbounds[0], xbounds[1], res)
+    y = np.linspace(ybounds[0], ybounds[1], res)
+    X, Y = np.meshgrid(x, y, copy=False)
+    return X, Y, f(X + 1.0j * Y)
+
+def plot_poly_real_mayavi(p, xbounds=(-1, 1), ybounds=(-1, 1), res=401):
+    X, Y, vals = get_vals(p, xbounds, ybounds, res)
+    ml.mesh(X, Y, vals.real)
+    ml.show()
+
+def plot_poly_imag_mayavi(p, xbounds=(-1, 1), ybounds=(-1, 1), res=401):
+    X, Y, vals = get_vals(p, xbounds, ybounds, res)
+    ml.mesh(X, Y, vals.imag)
+    ml.show()
+
+def plot_poly_both_mayavi(p, xbounds=(-1, 1), ybounds=(-1, 1), res=401):
+    X, Y, vals = get_vals(p, xbounds, ybounds, res)
+    vals[np.absolute(vals)>2] = np.nan
+    ml.mesh(X, Y, vals.real)
+    ml.mesh(X, Y, vals.imag)
+    ml.show()
+
+def plot_poly_real_matplotlib(p, xbounds=(-1, 1), ybounds=(-1, 1), res=401):
+    X, Y, vals = get_vals(p, xbounds, ybounds, res)
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
+    ax.plot_surface(X, Y, vals.real)
+    plt.show()
+
+def plot_poly_imag_matplotlib(p, xbounds=(-1, 1), ybounds=(-1, 1), res=401):
+    X, Y, vals = get_vals(p, xbounds, ybounds, res)
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
+    ax.plot_surface(X, Y, vals.imag)
+    plt.show()
+
+def plot_poly_both_matplotlib(p, xbounds=(-1, 1), ybounds=(-1, 1), res=401):
+    X, Y, vals = get_vals(p, xbounds, ybounds, res)
+    vals[np.absolute(vals)>2] = np.nan
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
+    ax.plot_surface(X, Y, vals.real)
+    ax.plot_surface(X, Y, vals.imag)
+    plt.show()
+
+def nroot_real_matplotlib(n, res=101):
     x = np.linspace(-1, 1, res)
     X, Y = np.meshgrid(x, x, copy=False)
     Z = X + 1.0j * Y
@@ -27,7 +73,7 @@ def plot_real_part_matplotlib(n, res=101):
         ax.plot_surface(X[x<0], Y[x<0], real[x<0])
     plt.show()
 
-def plot_imag_part_matplotlib(n, res=101):
+def nroot_imag_matplotlib(n, res=101):
     x = np.linspace(-1, 1, res)
     X, Y = np.meshgrid(x, x, copy=False)
     Z = X + 1.0j * Y
@@ -47,7 +93,7 @@ def plot_imag_part_matplotlib(n, res=101):
         ax.plot_surface(X[x<0], Y[x<0], imag[x<0])
     plt.show()
 
-def plot_real_part_mayavi(n, res=401):
+def nroot_real_mayavi(n, res=401):
     x = np.linspace(-1, 1, res)
     X, Y = np.meshgrid(x, x, copy=False)
     Z = X + 1.0j * Y
@@ -84,9 +130,12 @@ def plot_imag_part_mayavi(n, res=401):
     ml.show()
 
 if __name__ == '__main__':
+    p = np.poly1d([1, 0, 0, 0, 1])
     if use_mayavi:
         plot_real_part_mayavi(5)
         plot_imag_part_mayavi(5)
+        plot_poly_both_mayavi(p)
     else:
         plot_real_part_matplotlib(5)
         plot_imag_part_matplotlib(5)
+        plot_poly_both_matplotlib(p)
