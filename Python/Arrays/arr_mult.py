83b58c33930da6d51897ce049615d46d44cd7326
remifuhriman <remi.fuhriman@gmail.com>
2014-05-02 13:55:36 -0600

    changed order of variables so they were in alphabetical order.

--- Python/Arrays/arr_mult.py
+++ Python/Arrays/arr_mult.py
@@ -2,10 +2,10 @@ def arrmul(A,B):
     new = []
     for i in range(len(A)):
         newrow = []
-        for k in range(len(B[0])):
+        for j in range(len(B[0])):
             tot = 0
-            for j in range(len(B)):
-                tot += A[i][j] * B[j][k]
+            for k in range(len(B)):
+                tot += A[i][k] * B[k][j]
             newrow.append(tot)
         new.append(newrow)
     return new
