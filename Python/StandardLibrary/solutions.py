3341c33b8c5866198a58f48b5c8c92a83316a063
Ryan Grout <rgrout@math.byu.edu>
2014-05-02 18:22:02 -0600

    update solutions
    removed unused file.

--- Python/StandardLibrary/solutions.py
+++ Python/StandardLibrary/solutions.py
@@ -13,17 +13,6 @@ def time_func(f, args=(), kargs={}, repeat=3, number=100):
     except:
         T.print_exc()
 
-def pickle_obj():
-    j = ['5', 4, 2, 1, 'Python', 99918]
-
-    with open('out.pkl', 'w') as f:
-        pickle.dump(j, f)
-
-    with open('out.pkl', 'r') as f:
-        a = pickle.load(f)
-
-    print a == j
-
 def rot_list(l):
     for i in xrange(len(l)):
         l.insert(0, l.pop())
