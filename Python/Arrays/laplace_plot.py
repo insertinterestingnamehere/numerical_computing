8c39912d2912a77f77bad3987c8fc7607c6e1430
remifuhriman <remi.fuhriman@gmail.com>
2014-05-05 18:56:35 -0600

    Created python file to produce plot for problem 3

---
+++ Python/Arrays/laplace_plot.py
@@ -0,0 +1,17 @@
+from matplotlib import pyplot as plt 
+from mpl_toolkits.mplot3d import Axes3D 
+n = 100
+tol = .0001	
+U = np.ones ((n, n))
+U [:,0] = 100 # set north boundary condition
+U [:,-1] = 100 # set south boundary condition
+U [0] = 0 # set west boundary condition
+U [-1] = 0 # set east boundary condition
+laplace(U, tol) # U has been changed in place
+x = np.linspace(0, 1, n)
+y = np.linspace(0, 1, n)
+X, Y = np.meshgrid(x, y)
+fig = plt.figure()
+ax = fig.gca(projection = '3d')
+ax.plot_surface (X, Y, U, rstride=5)
+plt.show()
