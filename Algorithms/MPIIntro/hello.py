bca768f7880465173883688318fb194863f5916e
Ryan Grout <rgrout@math.byu.edu>
2014-05-28 00:34:49 -0600

    make code more explicit
    use better coding style.

--- Algorithms/MPIIntro/hello.py
+++ Algorithms/MPIIntro/hello.py
@@ -4,4 +4,4 @@ from mpi4py import MPI
 COMM = MPI.COMM_WORLD
 RANK = COMM.Get_rank()
 
-print "Hello world! I'm process number {RANK}.".format(**locals())
+print "Hello world! I'm process number {}.".format(rank)
