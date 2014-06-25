7289e7c52f197f9853dbe5016190b7e2449d9d33
Abraham Frandsen <abefrandsen@gmail.com>
2014-06-25 16:09:55 -0600

    More revisions to pandas lab.

--- Python/pandas/PandasSolutions.py
+++ Python/pandas/PandasSolutions.py
@@ -1,11 +1,13 @@
 import pandas as pd
 import numpy as np
 
-#prob 1
+# Series Problem
 s1 = pd.Series(-3, index=range(2, 11, 2))
 s2 = pd.Series({'Bill':31, 'Sarah':28, 'Jane':34, 'Joe':26})
 
-#prob 2
-with 1,00, I got ranges of -4 to 12 with 5 big peaks/troughs, -8 to 9 with 5 big peaks/troughs, and -8 to 9 to -40 with 3 big peaks/troughs
-with 1,000, I got ranges of -20 to 40 with 4 big peaks/troughs, -10 to 50 with 5 big peaks/troughs, and 20 to -80 with 1 big peak and trough
-with 10,000 I got ranges of -50 to 110 with 2 big peaks/troughs, -30 to 70 with 13 big peaks/troughs, and -20 to 80 with two big peaks/troughs
+# SQL SELECT problem
+studentInfo[(studentInfo['Age']>19)&(studentInfo['Sex']=='M')][['ID', 'Name']]
+
+# SQL JOIN problem
+pd.merge(studentInfo[studentInfo['Sex']=='M'], otherInfo, on='ID')[['ID', 'Age', 'GPA']]
+
