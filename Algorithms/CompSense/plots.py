7340e7d54efe8710e54ecfb709b3eb3bff404a3c
Ryan Grout <rgrout@math.byu.edu>
2014-08-16 19:50:18 -0600

    update plotting script

--- Algorithms/CompSense/plots.py
+++ Algorithms/CompSense/plots.py
@@ -4,7 +4,7 @@ from matplotlib import pyplot as plt
 
 import numpy as np
 from cvxopt import matrix, solvers
-import pyfftw
+from pyfftw.interfaces import scipy_fftpack as fftpack
 import math
 import scipy.misc
 from scipy import linalg as la
@@ -65,7 +65,7 @@ def sparse():
     m = 5
     s = np.zeros(100)
     s[np.random.permutation(100)[:m]] = np.random.random(m)
-    z = np.random.random_integers(0,high=1,size=(100,100)).dot(s)
+    z = np.random.random_integers(0, high=1, size=(100,100)).dot(s)
     plt.subplot(211)
     plt.plot(s)
     plt.subplot(212)
@@ -78,14 +78,14 @@ def incoherent():
     s = np.random.random((50,50))
     mask = s < .98
     s[mask] = 0
-    fs = pyfftw.interfaces.scipy_fftpack.fft2(s)
+    fs = fftpack.fft2(s)
     fs = np.abs(fs)
     plt.subplot(121)
-    plt.imshow(1-s,cmap=plt.cm.Greys_r, interpolation='nearest')
+    plt.imshow(1-s, cmap=plt.cm.Greys_r, interpolation='nearest')
     plt.xticks([])
     plt.yticks([])
     plt.subplot(122)
-    plt.imshow(fs,cmap=plt.cm.Greys_r, interpolation='nearest')
+    plt.imshow(fs, cmap=plt.cm.Greys_r, interpolation='nearest')
     plt.xticks([])
     plt.yticks([])
     plt.savefig('incoherent.pdf')
@@ -100,16 +100,16 @@ def reconstruct():
         R[27-2*i, -2-i] = 1.
     R[16,9:22] = 1.
     ncols, nrows = R.shape
-    n = ncols*nrows
+    n = ncols * nrows
     m = n / 4
     # generate DCT measurement matrix
-    D1 = math.sqrt(1./8)*pyfftw.interfaces.scipy_fftpack.dct(np.eye(n), axis=0)[np.random.permutation(n)[:m]]
+    D1 = math.sqrt(1./8) * fftpack.dct(np.eye(n), axis=0)[np.random.permutation(n)[:m]]
 
     # create measurements
     b = D1.dot(R.flatten())
 
-    rec_sig = l1Min(D1,b).reshape((nrows,ncols))
-    rec_sig2 = l2Min(D1,b).reshape((nrows,ncols))
+    rec_sig = l1Min(D1, b).reshape((nrows, ncols))
+    rec_sig2 = l2Min(D1, b).reshape((nrows, ncols))
     plt.subplot(1,3,1)
     plt.imshow(R, cmap=plt.cm.Greys_r, interpolation='nearest')
     plt.xticks([])
@@ -124,9 +124,10 @@ def reconstruct():
     plt.xticks([])
     plt.yticks([])
 
-    plt.savefig('reconstruct')
+    plt.savefig('reconstruct.pdf')
     plt.clf()
-
-sparse()
-incoherent()
-reconstruct()
+    
+if __name__ == "__main__":
+    sparse()
+    incoherent()
+    reconstruct()
