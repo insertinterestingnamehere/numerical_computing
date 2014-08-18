1ce65484885de54ca9c85190ce26724140f71e0f
Ryan Grout <rgrout@math.byu.edu>
2014-08-18 15:29:52 -0600

    update plots
    Use agg backend to rasterize the plots.  This reduces filesize.

--- Algorithms/FFT/plots.py
+++ Algorithms/FFT/plots.py
@@ -5,6 +5,8 @@
 ## TODO: update plots and verify
 import matplotlib
 matplotlib.rcParams = matplotlib.rc_params_from_file('../../matplotlibrc')
+# needed because of the number of points cairo can plot is limited
+matplotlib.rcParams['backend'] = 'agg'
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -16,63 +18,63 @@ from pyfftw.interfaces import scipy_fftpack as pyfft
 def pulseramp():
     rate, mywave = wavfile.read('pulseramp.wav')
     plt.plot(mywave)
-    plt.savefig('pulseramp.pdf')
+    plt.savefig('pulseramp.png')
     plt.clf()
     
 #plots spectrum of sine wave
-def plot_sine_spec():
+def sine_spec():
     samplerate = 44100 # 44100 samples per second
     freq = 1760 # We’re going to produce a 1760 Hz sine wave ...
     length = 2 # ... which will last for 2 seconds.
     stepsize = freq * 2*np.pi/samplerate
     sig = np.sin(np.arange(0, stepsize*length*samplerate, stepsize))     
     sig = np.float32(sig)
-    fsig = anfft.fft(sig)
+    fsig = pyfft.fft(sig)
     plt.plot(np.absolute(fsig))
-    plt.savefig('sinespec.pdf')
+    plt.savefig('sinespec.png')
     plt.clf()
     
 #plots spectrum of tada.wav
-def plot_tada_spec():
+def tada_spec():
     rate, sig = wavfile.read('tada.wav')
     sig = np.float32(sig)
-    fsig = anfft.fft(sig.T).T
+    fsig = pyfft.fft(sig.T).T
     plt.plot(np.absolute(fsig))
-    plt.savefig('tadaspec.pdf')
+    plt.savefig('tadaspec.png')
     plt.clf()
 
 #plots left half of spectrum of tada.wav
-def plot_tada_spec_left():
+def tada_spec_left():
     rate, sig = wavfile.read('tada.wav')
     sig = np.float32(sig)
-    fsig = anfft.fft(sig.T).T
+    fsig = pyfft.fft(sig.T).T
     f = np.absolute(fsig)
     plt.plot(f[0:f.shape[0]/2,:])
-    plt.savefig('tadaspec2.pdf')
+    plt.savefig('tadaspec2.png')
     plt.clf()
 
-def plot_saw_spec():
+def saw_spec():
     rate, data = wavfile.read('saw.wav')
     fsig = fft(data)
     f = np.absolute(fsig)
     plt.plot(f[0:f.shape[0]/2])
-    plt.savefig('sawspec.pdf')
+    plt.savefig('sawspec.png')
     plt.clf()
 
-def plot_down_saw_spec():
+def down_saw_spec():
     rate, data = wavfile.read('down_saw.wav')
     fsig = fft(data)
     f = np.absolute(fsig)
     plt.plot(f[0:f.shape[0]/2])
-    plt.savefig('sawspecdown.pdf')    
+    plt.savefig('sawspecdown.png')    
     plt.clf()
     
-def plot_down_saw_spec_correct():
+def down_saw_spec_correct():
     rate, in_sig = wavfile.read('saw.wav')
     old_rate = 44100
     new_rate = 22050
     in_sig = np.float32(in_sig)
-    fin = anfft.fft(in_sig)
+    fin = pyfft.fft(in_sig)
     nsiz = np.floor(in_sig.size*new_rate/old_rate)
     nsizh = np.floor(nsiz/2)
     fout = np.zeros(nsiz)
@@ -81,8 +83,16 @@ def plot_down_saw_spec_correct():
     fout[nsiz-nsizh+1:] = np.conj(np.flipud(fout[1:nsizh]))
     f = np.absolute(fout)
     plt.plot(f[0:f.shape[0]/2])
-    plt.savefig('sawdownspec.pdf')
+    plt.savefig('sawdownspec.png')
     plt.clf()
     
 if __name__ == "__main__":
     pulseramp()
+    sine_spec()
+    tada_spec()
+    tada_spec_left()
+    saw_spec()
+    down_saw_spec()
+    down_saw_spec_correct()
+    
+    
