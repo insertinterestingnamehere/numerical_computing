e0a56a06780ddf21fa8793dd175acd4ea74c5be7
Ryan Grout <rgrout@math.byu.edu>
2014-08-18 16:11:58 -0600

    update plotting scripts

--- Algorithms/float/plots.py
+++ Algorithms/float/plots.py
@@ -4,42 +4,8 @@ import matplotlib
 matplotlib.rcParams = matplotlib.rc_params_from_file('../../matplotlibrc')
 from matplotlib import pyplot as plt
 
-def sqrt32(A, reps):
-    Ac = A.copy()
-    I = Ac.view(dtype=np.int32)
-    I >>= 1
-    I += (1<<29) - (1<<22) - 0x4C000
-    for i in xrange(reps):
-        Ac = .5 *(Ac + A / Ac)
-    return Ac
+from solutions import sqrt64
 
-def sqrt64(A, reps):
-    Ac = A.copy()
-    I = Ac.view(dtype=np.int64)
-    I >>= 1
-    I += (1<<61) - (1<<51)
-    for i in xrange(reps):
-        Ac = .5 *(Ac + A / Ac)
-    return Ac
-
-# These do the same thing as the cython functions for the inverse square root.
-def invsqrt32(A, reps):
-    Ac = A.copy()
-    if 0 < reps:
-        Ac2 = A.copy()
-        Ac2 /= - 2
-        Ac3 = np.empty_like(Ac)
-    I = Ac.view(dtype=np.int32)
-    I >>= 1
-    I *= -1
-    I += 0x5f3759df #hexadecimal representation of the constant
-    for j in xrange(reps):
-        Ac3[:] = Ac
-        Ac3 *= Ac
-        Ac3 *= Ac2
-        Ac3 += 1.5
-        Ac *= Ac3
-    return Ac
 
 def invsqrt64(A, reps):
     Ac = A.copy()
@@ -59,17 +25,37 @@ def invsqrt64(A, reps):
         Ac *= Ac3
     return Ac
 
-X = np.linspace(0, 3, 501)
-plt.plot(X, sqrt64(X, 0), X, np.sqrt(X))
-plt.savefig("sqrt0.pdf")
-plt.cla()
-plt.plot(X, sqrt64(X, 1), X, np.sqrt(X))
-plt.savefig("sqrt1.pdf")
-plt.cla()
-X = np.linspace(.1, 3, 291)
-plt.plot(X, invsqrt64(X, 0), X, 1./np.sqrt(X))
-plt.savefig("invsqrt0.pdf")
-plt.cla()
-plt.plot(X, invsqrt64(X, 1), X, 1./np.sqrt(X))
-plt.savefig("invsqrt1.pdf")
-plt.cla()
+
+def sqrt0():
+    X = np.linspace(0, 3, 501)
+    plt.plot(X, sqrt64(X, 0), X, np.sqrt(X))
+    plt.savefig("sqrt0.pdf")
+    plt.clf()
+
+
+def sqrt1():
+    X = np.linspace(0, 3, 501)
+    plt.plot(X, sqrt64(X, 1), X, np.sqrt(X))
+    plt.savefig("sqrt1.pdf")
+    plt.clf()
+
+    
+def invsqrt0():
+    X = np.linspace(.1, 3, 291)
+    plt.plot(X, invsqrt64(X, 0), X, 1./np.sqrt(X))
+    plt.savefig("invsqrt0.pdf")
+    plt.clf()
+
+
+def invsqrt1():
+    X = np.linspace(.1, 3, 291)
+    plt.plot(X, invsqrt64(X, 1), X, 1./np.sqrt(X))
+    plt.savefig("invsqrt1.pdf")
+    plt.clf()
+
+    
+if __name__ == "__main__":
+    sqrt0()
+    sqrt1()
+    invsqrt0()
+    invsqrt1()
