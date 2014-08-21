b1091b01d79f2811190d6ba27710e8db46416dd2
Ryan Grout <rgrout@math.byu.edu>
2014-08-21 16:49:36 -0600

    remove two files that don't need to be

--- Algorithms/QR/stability.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-from numpy.random import rand
-from scipy import linalg as la
-
-Q, X = la.qr(rand(500,500)) # create a random orthonormal matrix:
-R = np.triu(rand(500,500)) # create a random upper triangular matrix
-A = np.dot(Q,R) # Q and R are the exact QR decomposition of A
-Q1, R1 = la.qr(A) # compute QR decomposition of A
