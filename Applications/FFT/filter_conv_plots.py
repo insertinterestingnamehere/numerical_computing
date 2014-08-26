b1a97bd47499cb85c1d66d22976aa9e8cc714dfd
Ryan Grout <rgrout@math.byu.edu>
2014-08-26 13:50:36 -0600

    update plotting script

--- Applications/FFT/filter_conv_plots.py
+++ Applications/FFT/filter_conv_plots.py
@@ -9,33 +9,31 @@ import matplotlib.pyplot as plt
 # Switch backends to render PNG images.
 plt.switch_backend("Agg")
 
-import scipy as sp
+import numpy as np
 from scipy.io import wavfile
 from scipy import fftpack as ft
 
 #plots Noisysignal1.wav
-def plot_noise():
+def noise():
     plt.close('all')
     rate, sig = wavfile.read('Noisysignal1.wav')
-    plt.figure()
     plt.plot(sig[0:sig.shape[0]/2])
     plt.savefig('noisy.png')
+    plt.clf()
     
 #plots lect half of spectrum of 
 #Noisysignal1.wav    
-def plot_noise_spec():
-    plt.close('all')
+def noise_spec():
     rate, sig = wavfile.read('Noisysignal1.wav')
-    sig = sp.float32(sig)
+    sig = sig.astype('float32')
     fsig = ft.fft(sig.T).T
-    f = sp.absolute(fsig)
-    plt.figure()
+    f =  np.absolute(fsig)
     plt.plot(f[0:f.shape[0]/2])
     plt.savefig('noisyspec.png')
+    plt.clf()
     
 #plots cleaned noisy signal
-def plot_cleaned_signal():
-    plt.close('all')
+def cleaned_signal():
     rate,data = wavfile.read('Noisysignal1.wav')
     fsig = ft.fft(data,axis = 0)
     for j in xrange(10000,20000):
@@ -43,13 +41,13 @@ def plot_cleaned_signal():
         fsig[-j]=0
 
     newsig = ft.ifft(fsig)
-    newsig = sp.real(newsig)
-    newsig = sp.int16(newsig/sp.absolute(newsig).max() * 32767)    
-    plt.figure()
+    newsig = newsig.real
+    newsig = (newsig/np.absolute(newsig).max()*32767).astype('int16')
     plt.plot(newsig[0:newsig.shape[0]/2])
     plt.savefig('Cleanedsignal.png')
+    plt.clf()
 
 if __name__ == '__main__':
-    plot_noise()
-    plot_noise_spec()
-    plot_cleaned_signal()
+    noise()
+    noise_spec()
+    cleaned_signal()
