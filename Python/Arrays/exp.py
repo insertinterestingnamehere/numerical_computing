73acc4a4e84cd3f0cca3e641bb007fa85edf1d7e
Rachel Suggs <1oldkingcoel@gmail.com>
2014-09-11 10:36:01 -0600

    minor revisions

--- Python/Arrays/exp.py
+++ Python/Arrays/exp.py
@@ -1,6 +1,6 @@
 from scipy.misc import factorial
 def exp(a, n = 25):
-	# Constructs an array in reverse order from n to 0.
+	# Construct an array in reverse order from n to 0.
 	A = np.arange(n, -1, -1)
 	# Use broadcasting to compute coefficients
 	p = 1. / factorial(A) 
