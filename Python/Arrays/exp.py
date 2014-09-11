af151e480b5f7947f407a3b85cdced83bf6a4bc4
Rachel Suggs <1oldkingcoel@gmail.com>
2014-09-11 06:25:01 -0600

    First edit. Focus on organization and writing.

--- Python/Arrays/exp.py
+++ Python/Arrays/exp.py
@@ -1,8 +1,8 @@
 from scipy.misc import factorial
-def exp(x, n = 25):
-	# np.arange(n, -1, -1) starts at n and constructs an array in 
-	# reverse order from n to 0.
+def exp(a, n = 25):
+	# Constructs an array in reverse order from n to 0.
 	A = np.arange(n, -1, -1)
-	p = 1. / factorial(A) # compute coefficients
+	# Use broadcasting to compute coefficients
+	p = 1. / factorial(A) 
 	P = np.poly1d(p) # make polynomial object
-	return P(x)
+	return P(a)
