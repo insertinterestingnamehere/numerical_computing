db67d9c75b5af9598bd9a944be50a04b43eedc13
Rachel Suggs <1oldkingcoel@gmail.com>
2014-09-15 10:24:39 -0600

    eliminated timeit problems; fixed table placement

--- Python/Arrays/exp.py
+++ Python/Arrays/exp.py
@@ -1,8 +1,8 @@
 from scipy.misc import factorial
 def exp(a, n = 25):
 	# Construct an array in reverse order from n to 0.
-	A = np.arange(n, -1, -1)
+	integers = np.arange(n, -1, -1)
 	# Use broadcasting to compute coefficients
-	p = 1. / factorial(A) 
-	P = np.poly1d(p) # make polynomial object
-	return P(a)
+	coefficients = 1. / factorial(integers) 
+	poly = np.poly1d(coefficients) # make polynomial object
+	return poly(a)
