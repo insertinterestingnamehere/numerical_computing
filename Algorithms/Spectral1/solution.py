036b4ed75010d55ce9ecd0a5f28be7078d8e0b60
Joshua Lytle <joshualyte@gmail.com>
2014-10-15 15:49:48 -0600

    Major changes to the first pseudospectral lab. Added some code vectorizing the construction of the differentiation matrix. Added some code solving the nonlinear boundary value problem resulting from the use of the calculus of variations to minimize the area of a surface of revolution.

--- Algorithms/Spectral1/solution.py
+++ Algorithms/Spectral1/solution.py
@@ -3,7 +3,7 @@ import numpy as np
 
 
 
-def cheb(N):
+def cheb_loop(N):
 	def p(j1):
 		if (j1==0 or j1 == N): return 2.
 		else: return 1.
@@ -24,3 +24,24 @@ def cheb(N):
 	D[N,N] = -(1.+2.*N**2.)/6.
 	
 	return D,x
+
+
+def cheb_vectorized(N):
+	x =  np.cos((np.pi/N)*np.linspace(0,N,N+1))
+	x.shape = (N+1,1)
+	lin = np.linspace(0,N,N+1)
+	lin.shape = (N+1,1)
+	
+	c = np.ones((N+1,1))
+	c[0], c[-1] = 2., 2.
+	c = c*(-1.)**lin
+	X = x*np.ones(N+1) # broadcast along 2nd dimension (columns)
+	
+	dX = X - X.T
+	
+	D = (c*(1./c).T)/(dX + np.eye(N+1))
+	D  = D - np.diag(np.sum(D.T,axis=0))
+	x.shape = (N+1,)
+	# Here we return the differentation matrix and the Chebychev points, 
+	# numbered from x_0 = 1 to x_N = -1
+	return D, x
