44f9c70038c1d829a0385bccc54d0c8e3b1b2be5
Abraham Frandsen <abefrandsen@gmail.com>
2014-11-12 19:46:54 -0700

    Updated solutions and plotting file, re-generated the figures.

--- Applications/FacialRecognition/plotting.py
+++ Applications/FacialRecognition/plotting.py
@@ -1,13 +1,50 @@
 import matplotlib
 matplotlib.rcParams = matplotlib.rc_params_from_file('../../matplotlibrc')
-
 import numpy as np
-
-import solutions as sol
-
 from scipy.misc import imsave
+from scipy.linalg import svd
+from os import walk
+from scipy.ndimage import imread
 
+def getFaces(path="./faces94"):
+    # these are the dimensions of the images
+    w = 200
+    h = 180
+    
+    # traverse the directory, get one image per subdirectory
+    faces = []
+    for (dirpath, dirnames, filenames) in walk(path):
+        for f in filenames:
+            if f[-3:]=="jpg": # only get jpg images
+                # load image, convert to grayscale, flatten into vector
+                face = imread(dirpath+"/"+f).mean(axis=2).ravel() 
+                faces.append(face)
+                break
+                
+    # put all the face vectors column-wise into a matrix
+    F = np.array(faces).T
+    return F
 
+def plotMeanFace(mu):
+    imsave("meanFace.png", mu.reshape((200, 180)))
+
+def plotDiffFaces(A):
+    for j, i in enumerate([0,20,50]):
+        imsave("differenceFace{0}.png".format(j),A[:,i].reshape((200,180)))
+
+def plotEigenFaces(U):
+    for i in xrange(3):
+        imsave("eigenface{0}.png".format(i),U[:,i].reshape((200,180)))
+
+def plotReconstructed(U, A, mu):
+    imsave("rebuiltAll.png", (A[:,20]+mu).reshape((200,180)))
+    s = U.shape[1]
+    r1 = U[:,:s/2].dot(U[:,:s/2].T.dot(A[:,20])) + mu
+    r2 = U[:,:s/4].dot(U[:,:s/4].T.dot(A[:,20])) + mu
+    imsave("rebuiltHalf.png", r1.reshape((200,180)))
+    imsave("rebuiltFourth.png", r2.reshape((200,180)))
+
+# below are the plotting functions from a previous version
 def checkMeanImage(faces):
     """
     Plot the mean face of a facial recognition database
@@ -44,10 +81,12 @@ def checkRebuildImage(faces,image):
     
     
 if __name__ == "__main__":
-    faces = sol.FacialRec("./faces94/male/",30)
-    images,dim = sol.getImages("./faces94/male/")
-    
-    checkMeanImage(faces)
-    checkDifferenceFaces(faces)
-    checkEigenFaces(faces)
-    checkRebuildImage(faces,images[0])
+    F = getFaces(path="../../../faces94")
+    print F.shape
+    mu = F.mean(axis=1)
+    A = F - mu.reshape((len(mu), 1))
+    U,Sig,Vh = svd(A, full_matrices=False)
+    plotMeanFace(mu)
+    plotDiffFaces(A)
+    plotEigenFaces(U)
+    plotReconstructed(U, A, mu)
