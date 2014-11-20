256d3e6418caf91470b47a11206fccc88ce869bc
Ryan Grout <rgrout@math.byu.edu>
2014-11-20 11:16:55 -0700

    update all the plots

--- Applications/IsingModel/isingmodel.py
+++ Applications/IsingModel/isingmodel.py
@@ -3,10 +3,6 @@ import scipy.stats as stats
 import matplotlib.pyplot as plt
 import math
 
-binomial = np.random.binomial
-exp = math.exp
-randint = np.random.randint
-
 def initialize(n, p=.5):
     state = stats.bernoulli.rvs(p, size=(n ** 2)).astype(float)
     state = state * 2 - 1
@@ -27,38 +23,32 @@ def proposedEnergy(state, i, j, energy):
     return energy + 2 * S * tmp
 
 
-def accept(energy, newEnergy, beta=10):
-    if newEnergy <= energy or binomial(1, exp(-beta * (newEnergy - energy))) == 1:
-        accepted = True
-    return False
-
-
 def mcmc(n, beta, burnin=100000, n_samples=5000):
     state = initialize(n)
     energy = computeEnergy(state)
     logprobs = np.zeros(burnin + n_samples)
     randvals = np.random.randint(0, n**2, size=(burnin+n_samples))
+    exp = math.exp
+    binomial = np.random.binomial
     print "Burning in ... ({} samples)".format(burnin)
     for k in xrange(burnin):
         logprobs[k] = -beta * energy
         i = randvals[k]/n
         j = randvals[k]%n
-        newEnergy = proposedEnergy(state, i, j, energy)
-        accepted = accept(energy, newEnergy, beta)
-        if accepted:
+        newEnergy = proposedEnergy(state, i, j, energy) - energy
+        if newEnergy <= 0 or binomial(1, exp(-beta * (newEnergy))) == 1:
             state[i, j] *= -1
-            energy = newEnergy
+            energy = newEnergy + energy
     samples = []
     print "Sampling ... ({} samples)".format(n_samples)
     for k in xrange(n_samples):
         logprobs[k + burnin] = -beta * energy
         i = randvals[burnin+k]/n
         j = randvals[burnin+k]%n
-        newEnergy = proposedEnergy(state, i, j, energy)
-        accepted = accept(energy, newEnergy, beta)
-        if accepted:
+        newEnergy = proposedEnergy(state, i, j, energy) - energy
+        if newEnergy <= 0 or binomial(1, exp(-beta * (newEnergy))) == 1:
             state[i, j] *= -1
-            energy = newEnergy
+            energy = newEnergy + energy
         if not k % 100:
             samples.append(state.copy())
     return samples, logprobs
--- Applications/IsingModel/plots.py
+++ Applications/IsingModel/plots.py
@@ -2,11 +2,24 @@ import matplotlib
 matplotlib.rcParams = matplotlib.rc_params_from_file('../../matplotlibrc')
 
 import isingmodel
+import scipy.misc as spmisc
 import matplotlib.pyplot as plt
 
 def initialize():
     spinconfig = isingmodel.initialize(100)
-    plt.imshow(spinconfig)
-    plt.savefig('init.pdf')
+    spmisc.imsave("init.pdf", spinconfig)
     
-initialize()
+def beta(n, beta=1):
+    samples, logprobs = isingmodel.mcmc(n, beta, n_samples=5000)
+    
+    stem = str(beta).replace(".", "_")
+    plt.plot(logprobs)
+    plt.savefig("beta" + stem + "_logprobs.pdf")
+    plt.clf()
+    spmisc.imsave("beta" + stem + ".pdf", samples[-1])
+    
+    
+if __name__ == "__main__":    
+    initialize()
+    beta(100, beta=1)
+    beta(100, beta=.2)
