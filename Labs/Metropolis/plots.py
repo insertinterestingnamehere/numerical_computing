aa73b35fb6b7a7b4bd2dff17d17916fb2c32c701
Abraham Frandsen <abefrandsen@gmail.com>
2015-02-26 10:00:19 -0700

    Further revisions of metropolis lab.

--- Labs/Metropolis/plots.py
+++ Labs/Metropolis/plots.py
@@ -6,6 +6,12 @@ import metropolis
 import numpy as np
 import scipy.misc as spmisc
 import matplotlib.pyplot as plt
+from math import sqrt, exp, log
+import scipy.stats as st
+from matplotlib import pyplot as plt
+from scipy.stats import gaussian_kde
+
+scores = np.array([98,92,89,77,87,84,75,73,95,86,67,86,86,100,100,92,100,97,95,77,87,87,95,84,84,74,86,84,94])
 
 def initialize():
     spinconfig = isingmodel.initialize(100)
@@ -32,3 +38,55 @@ def samples_logs():
     plt.plot(logs)
     plt.savefig('logprobs.pdf')
     plt.clf()
+
+# let's do a metropolis sampler for the exam scores data
+alpha=3
+beta=50
+mu0 = 80
+sig20 = 16
+muprior=st.norm(loc=mu0, scale=sqrt(sig20))
+sig2prior = st.invgamma(alpha,scale=beta)
+def proposal(y, std):
+    return st.multivariate_normal.rvs(mean=y, cov=std*np.eye(len(y)))
+def propLogDensity(x):
+    return muprior.logpdf(x[0])+sig2prior.logpdf(x[1])+st.norm.logpdf(scores,loc=x[0],scale=sqrt(x[1])).sum()
+def metropolis(x0, s, n_samples):
+    """
+    Use the Metropolis algorithm to sample from posterior.
+    
+    Parameters
+    ----------
+    x0 : ndarray of shape (2,)
+        The first entry is mu, the second entry is sigma2
+    s : float > 0
+        The standard deviation parameter for the proposal function
+    n_samples : int
+        The number of samples to generate
+        
+    Returns
+    -------
+    draws : ndarray of shape (n_samples, 2)
+        The MCMC samples
+    logprobs : ndarray of shape (n_samples)
+        The log density of the samples
+    accept_rate : float
+        The proportion of proposed samples that were accepted
+    """
+    accept_counter = 0
+    draws = np.empty((n_samples,2))
+    logprob = np.empty(n_samples)
+    x = x0.copy()
+    for i in xrange(n_samples):
+        xprime = proposal(x,s)
+        u = np.random.rand(1)[0]
+        if log(u) <= propLogDensity(xprime) - propLogDensity(x):
+            accept_counter += 1
+            x = xprime
+        draws[i] = x
+        logprob[i] = propLogDensity(x)
+    return draws, logprob, accept_counter/float(n_samples)
+
+
+if __name__ == "__main__":
+    draws, lprobs, r = metropolis(np.array([40, 10], dtype=float), 20., 10000)
+    
