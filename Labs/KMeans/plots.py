b08efa4bfc6d37361c67d5337c7e10b4903a477b
Abraham Frandsen <abefrandsen@gmail.com>
2015-03-12 17:31:33 -0600

    Fixed everything up, should be ready now.

--- Labs/KMeans/plots.py
+++ Labs/KMeans/plots.py
@@ -7,9 +7,10 @@ from sklearn.datasets import load_iris
 from sklearn import decomposition
 from scipy import linalg as la
 
-from solutions import kmeans
+from solutions import kmeans, loadEarthquakes, clusterEarthquakes
 
 
+EARTHQUAKE_PATH = "../../../Earthquake_Data/20100{}.txt"
 
 iris = load_iris()
 X = iris.data
@@ -21,6 +22,31 @@ U,S,VT = la.svd(Y,full_matrices=False)
 Yhat = U[:,:2].dot(np.diag(S[:2]))
 p1, p2 = Yhat[:,0], Yhat[:,1]
 
+
+def plotEarthquakes():
+    lat, long = loadEarthquakes(EARTHQUAKE_PATH)
+    plt.scatter(long, lat, marker='.',s=5)
+    plt.xlim([-180,180])
+    plt.ylim([-90,90])
+    plt.xlabel("Longitude")
+    plt.ylabel("Latitude")
+    plt.savefig("earthquakes.png",dpi=150,frameon=False,transparent=True)
+    plt.clf()
+
+def plotEarthClusters():
+    lats, longs, m_lats, m_longs, labs = clusterEarthquakes(EARTHQUAKE_PATH)
+    for i in xrange(15):
+        msk=labs==i
+        c=np.random.rand(3)
+        plt.scatter(longs[msk],lats[msk],marker='.',color=c,s=5)
+    plt.scatter(m_longs*180/np.pi,m_lats*180/np.pi,marker='+',s=100,linewidths=2,color='k')
+    plt.xlim([-180,180])
+    plt.ylim([-90,90])    
+    plt.xlabel("Longitude")
+    plt.ylabel("Latitude")
+    plt.savefig("earthquake_clusters.png",dpi=110,frameon=False,transparent=True)
+    plt.clf()
+   
 def iris_pca():
     plt.scatter(p1,p2, marker='.', color='blue')
     plt.ylim([-4,5])
@@ -59,6 +85,7 @@ def iris_means():
     plt.xlabel("First Principal Component")
     plt.ylabel("Second Principal Component")
     plt.savefig("iris_means_1.pdf")
+    plt.clf()
     
     means,labs,i = kmeans(Yhat,3,init=means2)
     
@@ -81,6 +108,9 @@ def iris_means():
     plt.xlabel("First Principal Component")
     plt.ylabel("Second Principal Component")
     plt.savefig("iris_means_2.pdf")
+    plt.clf()
 
 if __name__ == "__main__":
+    #plotEarthClusters()
+    #plotEarthquakes()
     iris_means()
